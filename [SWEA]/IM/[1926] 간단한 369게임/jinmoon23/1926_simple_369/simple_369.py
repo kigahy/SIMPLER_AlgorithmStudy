44404aae8ad40aa2f4ed65f46c232ae01071be89
jinmoon23 <wlsansdlek1@me.com>
2024-08-04 21:23:24 +0900

    주석수정

--- [SWEA]/IM/[1926] 간단한 369게임/jinmoon23/1926_simple_369/simple_369.py
+++ [SWEA]/IM/[1926] 간단한 369게임/jinmoon23/1926_simple_369/simple_369.py
@@ -1,14 +1,12 @@
-'''
 
-'''
 def simple_369_game(N):
-    list_of_369 = ['33','36','39','63','66','69','93','96','99']
+    list_of_369 = ['33','36','39','63','66','69','93','96','99'] # 2개의 3 또ㄴ 6 또는 9를 식별할 경우를 대비하기 위한 list
     str_number_list = list(map(str,range(1,N+1)))
 
-    for index,string in enumerate(str_number_list):
+    for index,string in enumerate(str_number_list): # enumerate 메서드로 인덱스와 값에 접근
         if string in list_of_369:
-            str_number_list[index] = '--'
-            continue
+            str_number_list[index] = '--' # 박수 두 번을 출력해야 하는 경우를 먼저 확인
+            continue # 아래 코드를 실행시키지 않도록 continue로 반복문 제어
         if '3' in string or '6' in string or '9' in string:
             str_number_list[index] = '-'
 

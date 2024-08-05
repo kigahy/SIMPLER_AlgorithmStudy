97e2664672fd5ebe6fd9199fb0239ff612e66792
kimgahyeonn <in47821@gmail.com>
2024-08-05 16:59:28 +0900

    Update SWEA_9367.py

--- [SWEA]/IM/[9367] 점점 커지는 당근의 개수/gahyeonn/SWEA_9367.py
+++ [SWEA]/IM/[9367] 점점 커지는 당근의 개수/gahyeonn/SWEA_9367.py
@@ -7,14 +7,11 @@ for test_case in range(1, T+1) :
 
     max = 0
     count = 0
-    lst = []
-    j = 0
     for i in range(1, carrot) :
         if scale[i] - scale[i-1] == 1 :
             count += 1
-            
-        else :
-            count = 0
+
+            if max < scale[i] :
+                max = scale[i]
 
     print(max)
-

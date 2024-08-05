93fdf01996b6fbeb98201d066997917e6a80abe3
jinmoon23 <161567425+jinmoon23@users.noreply.github.com>
2024-08-05 17:29:36 +0900

    Update pop_2.py

--- [SWEA]/IM/[16268] 풍선팡2/jinmoon23/16268_pop_2/pop_2.py
+++ [SWEA]/IM/[16268] 풍선팡2/jinmoon23/16268_pop_2/pop_2.py
@@ -5,9 +5,8 @@ def find_max_flower_powder(matrix):
     dxy = [[-1,0],[0,1],[1,0],[0,-1]] # 위 / 우측 / 아래 / 좌측
     # dx = [-1,0,1,0]
     # dy = [0,1,0,-1]
-    final_compare_list = []
+    compare_list = []
     for i in range(N):
-        compare_list = []
         for j in range(M):
             dummy_list = []
             dummy_list.append(matrix[i][j])
@@ -17,11 +16,17 @@ def find_max_flower_powder(matrix):
                 if nx < 0 or ny < 0 or nx >= N or ny >= M: continue
                 dummy_list.append(matrix[nx][ny])
             compare_list.append(sum(dummy_list))
-        final_compare_list.append(max(compare_list))
-    return max(final_compare_list)
+    return max(compare_list)
 
 T = int(input())
 for test_case in range(1, T + 1):
     N,M = map(int,input().split())
     matrix = [list(map(int,input().split())) for _ in range(N)]
     print(f'#{test_case} {find_max_flower_powder(matrix)}')
+
+
+'''
+가현님 코드리뷰
+- delta에 각각 0을 설정하는 아이디어 좋습니다!
+- 
+'''

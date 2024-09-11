b6f280f28114e2fb03d89676353c12b14f238901
skdud5126 <skdud5126@gmail.com>
2024-09-11 22:18:05 +0900

    upload_nayoung

--- [PGS]/Advanced/여행경로/[여행경로]_nayoung.py
+++ [PGS]/Advanced/여행경로/[여행경로]_nayoung.py
@@ -1,15 +1,24 @@
+'''
+어떻게든 해보려고했지만..
+테케1,2는 맞지만 ... 채점은 1,2번 다 틀리네요... ㅠㅠ 악질문제 완탑입니다...
+제 생각 하다보니 테케에 맞춘 로직이다보니 엣지 케이스는 생각 못해서 틀린듯..?
+아무튼 제 역량은 여기까지입니더...흑
+
+'''
+
+
 def solution(tickets):
     def dfs(start, ans):
 
-        for i in range(len(road_dict[start])):
-            if road_dict[start][i] not in road_dict.keys():
-                ans.append(road_dict[start][i])
+        for i in range(len(road_dict[start])):  # 현재 출발지에서 갈 수 있는 모든 목적지 탐색하겠다.
+            if road_dict[start][i] not in road_dict.keys():  # 목적지가 존재하지 않을 경우
+                ans.append(road_dict[start][i])  # 해당 목적지 추가하고 리턴
                 return
 
-            if visited[start][i]:
+            if visited[start][i]:  # 방문하였다면 continue
                 continue
-            visited[start][i] = 1
-            ans.append(road_dict[start][i])
+            visited[start][i] = 1  # 방문 표시
+            ans.append(road_dict[start][i])  # root에 목적지 추가
             dfs(road_dict[start][i], ans)
 
 
